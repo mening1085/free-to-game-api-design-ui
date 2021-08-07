9fd0628fdd5778261f7ec1e559146111d06df1eb
mening1085 <mening1085@gmail.com>
2021-08-07 23:00:15 +0700

    add project

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,146 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- Navbar -->
+    <Navbar />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <!-- Content -->
+    <v-main class="bg">
+      <div class="container">
+        <v-row>
+          <!-- left -->
+          <v-col cols="8">
+            <div>
+              <h2>Recently Added</h2>
+            </div>
+
+            <v-card
+              color="#32383D"
+              elevation="2"
+              v-for="(item, i) in games.slice(0, 3)"
+              :key="i"
+              class="pa-4 mb-4"
+            >
+              <v-layout class="d-flex justify-end align-center">
+                <v-flex xs3 class="mx-3">
+                  <v-img
+                    style="border-radius: 4px"
+                    :src="item.thumbnail"
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7 class="mx-3 align-center">
+                  <div style="color: #aaa">
+                    <h3>{{ item.title }}</h3>
+                  </div>
+                  <div style="color: #798287" class="desc mb-1">
+                    {{ item.short_description }}
+                  </div>
+                  <div>
+                    <v-btn
+                      height="20"
+                      class="px-1"
+                      depressed
+                      small
+                      color="#aaa"
+                      dark
+                    >
+                      <span style="color: #32383D">
+                        {{ item.genre }}
+                      </span>
+                    </v-btn>
+                  </div>
+                </v-flex>
+                <v-flex xs2>
+                  <v-btn small depressed color="#4799EB" dark>
+                    FREE
+                  </v-btn>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-col>
+
+          <!-- right -->
+          <v-col cols="4">
+            <div>
+              <h2>Most Played Today</h2>
+            </div>
+            <v-card
+              class="mb-3"
+              elevation="2"
+              v-for="(item, i) in games.slice(0, 2)"
+              :key="i"
+            >
+              <v-img :src="item.thumbnail"></v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            class="pa-2"
+            v-for="(item, i) in games.slice(3, 100)"
+            :key="i"
+            cols="3"
+          >
+            <v-img :src="item.thumbnail"></v-img>
+          </v-col>
+        </v-row>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Navbar from "./components/Navbar.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    Navbar,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      games: null,
+    };
+  },
+
+  async created() {
+    var options = {
+      method: "GET",
+      url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+      headers: {
+        "x-rapidapi-key": "bf3353835emsh6fc47102e25e082p1bc844jsneccc6e65d0f1",
+        "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+    const res = await axios
+      .request(options)
+      .then(function(response) {
+        return response.data;
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+
+    this.games = res.slice(0, 99);
+    // this.games = res.slice(0, 10);
+  },
+
+  methods: {
+    // Function
+  },
 };
 </script>
+
+<style lang="css">
+.bg {
+  background: #272b30;
+  color: #aaa;
+}
+.desc {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-app-bar app color="#272B30" dark>
+      <div class="d-flex align-center">
+        <v-icon style="font-size: 56px;" class="mr-2">
+          mdi-gamepad-variant
+        </v-icon>
+        <div class="title">
+          Free to Game
+        </div>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div>
+        <v-text-field
+          outlined
+          hide-details
+          dense
+          placeholder="search"
+          prepend-inner-icon="mdi-magnify"
+        ></v-text-field>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.title {
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
